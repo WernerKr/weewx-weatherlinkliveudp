ab8f96d1f938ef8dc36d055613f0a3eca8d6ee31
Werner Krenn <93549501+WernerKr@users.noreply.github.com>
2023-11-21 16:18:27 +0100

    Add files via upload

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 # Installer file for WeatherLink Live (WeatherLinkLiveUDP) driver for WeeWX
 # Copyright 2020 Bastiaan Meelberg
-# Modified 2021/2022 Werner Krenn (leaf/soil/Extra2..4,ISS2,Wind,Rain,Batlevel,Signal)
+# Modified 2021/2023 Werner Krenn (leaf/soil/Extra2..4,ISS2,Wind,Rain,Batlevel,Signal)
 # Distributed under the terms of the GNU Public License (GPLv3)
 
 from setup import ExtensionInstaller
@@ -11,7 +11,7 @@ def loader():
 class weatherlinkliveudpInstaller(ExtensionInstaller):
     def __init__(self):
         super(weatherlinkliveudpInstaller, self).__init__(
-            version='0.5.1',
+            version='0.5.2',
             name='WeatherLinkLiveUDP',
             description='Periodically poll weather data from a WeatherLink Live device',
             author="Bastiaan Meelberg, Werner Krenn",
@@ -33,7 +33,25 @@ class weatherlinkliveudpInstaller(ExtensionInstaller):
                     '#did': '001D0A61F5E8',
                     'log': 0,
                 }
+                'RadiationDays': {
+                    'min_sunshine': '120',
+                    'sunshine_coeff': '0.95',
+                    'sunshine_min': '18',
+                    'sunshine_loop': '1',
+                    'rainDur_loop': '0',
+                    'sunshine_log': '0',
+                    'rainDur_log': '0',
+                    'rain2': '1',
+                    'sunshine2': '0',
+                    'sunshine2_loop': '1',
+                    'rainDur2_loop': '0',
+                    'sunshine2_log': '0',
+                    'rainDur2_log': '0',
+                },
             },
-            files=[('bin/user', ['bin/user/weatherlinkliveudp.py'])]
-
+            files=[
+                ('bin/user', [
+                    'bin/user/weatherlinkliveudp.py',
+                    'bin/user/sunrainduration.py',
+                ]),
         )
